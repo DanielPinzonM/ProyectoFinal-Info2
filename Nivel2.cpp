233adb17e29d9988b1e9ca280faf3196789fca76
DanielPinzonM <daniele.pinzon@udea.edu.co>
2024-06-02 13:58:17 -0500

    Se inicia la codificacion del nivel 2 del proyecto final

---
+++ Nivel2.cpp
@@ -0,0 +1,3 @@
+#include "Nivel2.h"
+
+Nivel2::Nivel2() {}
---
+++ Nivel2.h
@@ -0,0 +1,17 @@
+#ifndef NIVEL2_H
+#define NIVEL2_H
+
+#include <QGraphicsScene>
+#include <QWidget>
+#include <QList>
+#include <QKeyEvent>
+#include <QMouseEvent>
+
+class Nivel2 : public QWidget
+{
+    Q_OBJECT
+public:
+    Nivel2();
+};
+
+#endif // NIVEL2_H
