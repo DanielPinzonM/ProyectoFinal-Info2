3b1f9893650ab8b1bd9e452d949db2626ccad07b
DanielPinzonM <daniele.pinzon@udea.edu.co>
2024-06-02 21:06:28 -0500

    Se añade fondo y movimiento de la escena, junto con eventos de teclado

--- Nivel2.cpp
+++ Nivel2.cpp
@@ -1,3 +1,69 @@
 #include "Nivel2.h"
 
-Nivel2::Nivel2() {}
+Nivel2::Nivel2()
+{
+    Escena->setBackgroundBrush(QImage("Recursos/Fondo.png"));
+    Escena->setSceneRect(0,0,1,1);
+
+    Refresco = new QTimer();
+    connect(Refresco, SIGNAL(timeout()), this, SLOT(Actualizar()));
+    Refresco->start(16);
+}
+
+QGraphicsScene* Nivel2::getEscena()
+{
+    return Escena;
+}
+
+
+void Nivel2::AgregarTecla(QKeyEvent* event)
+{
+    if(event->key() == Qt::Key_D)
+    {
+        if(!event->isAutoRepeat())
+        {
+            Keys.insert(event->key());
+        }
+    }
+
+    if(event->key() == Qt::Key_A)
+    {
+        if(!event->isAutoRepeat())
+        {
+            Keys.insert(event->key());
+        }
+    }
+}
+
+void Nivel2::RemoverTecla(QKeyEvent* event)
+{
+    if(event->key() == Qt::Key_D)
+    {
+        if(!event->isAutoRepeat())
+        {
+            Keys.remove(event->key());
+        }
+    }
+    else if(event->key() == Qt::Key_A)
+    {
+        if(!event->isAutoRepeat())
+        {
+            Keys.remove(event->key());
+        }
+    }
+}
+
+void Nivel2::Actualizar()
+{
+    if(!(Keys.empty()))
+    {
+        if(Keys.contains(Qt::Key_D))
+        {
+            Escena->setSceneRect(Escena->sceneRect().x()+4,0,1,1);
+        }
+        if(Keys.contains(Qt::Key_A))
+        {
+            Escena->setSceneRect(Escena->sceneRect().x()-4,0,1,1);
+        }
+    }
+}
--- Nivel2.h
+++ Nivel2.h
@@ -4,14 +4,28 @@
 #include <QGraphicsScene>
 #include <QWidget>
 #include <QList>
+#include <QSet>
+#include <QTimer>
 #include <QKeyEvent>
 #include <QMouseEvent>
 
 class Nivel2 : public QWidget
 {
     Q_OBJECT
+private:
+    QGraphicsScene* Escena = new QGraphicsScene;
+    QSet<int> Keys;
+    QTimer* Refresco;
+
 public:
     Nivel2();
+    QGraphicsScene* getEscena();
+
+    void AgregarTecla(QKeyEvent* event);
+    void RemoverTecla(QKeyEvent* event);
+
+public slots:
+    void Actualizar();
 };
 
 #endif // NIVEL2_H
