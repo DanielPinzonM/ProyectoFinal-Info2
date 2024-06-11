93250c7c105abda0a95e53fdfa0b6f226bc3a6f9
NicolasVZ11 <142182818+NicolasVZ11@users.noreply.github.com>
2024-06-11 13:59:33 -0500

    se corrigen errores en el juego final y se agregan ultimos detalles

--- nivel1.cpp
+++ nivel1.cpp
@@ -12,7 +12,7 @@ Nivel1::Nivel1()
     Fondo->setPos(0,-3342);
     barco = new Barco();
     Escena->addItem(barco->GetImagen());
-    barco->GetImagen()->setPos(100,100);
+    barco->GetImagen()->setPos(500,400);
     agregarlimites();
     Actualizar.start(16);
     connect(&Actualizar, &QTimer::timeout, this, &Nivel1::Actualizarjuego);
@@ -34,7 +34,7 @@ QGraphicsScene* Nivel1::GetEscena() {
 
 void Nivel1::agregarlimites()
 {
-    QPen pen (Qt::black,3,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin);
+    QPen pen (Qt::transparent,3,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin);
 
     Paredes.append(Escena->addRect(0,0,1200,10,pen)); // arriba
     Paredes.append(Escena->addRect(0,640,1200,10,pen)); // abajo
@@ -47,6 +47,7 @@ void Nivel1::agregarlimites()
 void Nivel1::EventoTeclado(QKeyEvent* event)
 {
     QPointF pos = barco->GetImagen()->pos();
+    QPointF prevpos = barco->GetImagen()->pos();
 
     qreal rotationStep = 5; // Ángulo de rotación por paso
 
@@ -55,7 +56,7 @@ void Nivel1::EventoTeclado(QKeyEvent* event)
         // Movimiento hacia la derecha
         if (barco->GetImagen()->rotation() < 20)
         {
-        pos.setX(pos.x() + 5);
+        pos.setX(pos.x() + 7);
         }
         else if(barco->GetImagen()->rotation() > 20)
         {
@@ -67,14 +68,13 @@ void Nivel1::EventoTeclado(QKeyEvent* event)
     case Qt::Key_A:
         if (barco->GetImagen()->rotation() > -20)
         {
-        pos.setX(pos.x() - 5);
+        pos.setX(pos.x() - 7);
         }
         else if(barco->GetImagen()->rotation() < -20)
         {
         pos.setX(pos.x() - 10);
         }
-        // Movimiento hacia la izquierda
-        // Rotar el barco hacia la izquierda (en sentido antihorario) con el ángulo de rotación más lento
+
         barco->GetImagen()->setRotation(barco->GetImagen()->rotation() - rotationStep);
         break;
     case Qt::Key_W:
@@ -87,6 +87,7 @@ void Nivel1::EventoTeclado(QKeyEvent* event)
         break;
     }
 
+    barco->GetImagen()->setPos(pos); // Establecer nueva posición del barco
     // Limitar la rotación dentro del rango de -45 a 45 grados
     qreal currentRotation = barco->GetImagen()->rotation();
     if (currentRotation > 45) {
@@ -94,8 +95,14 @@ void Nivel1::EventoTeclado(QKeyEvent* event)
     } else if (currentRotation < -45) {
         barco->GetImagen()->setRotation(-45);
     }
+    for (int i = 0; i < Paredes.length(); i++) {
+        if (barco->GetImagen()->collidesWithItem(Paredes.at(i))) {
+        // Si hay colisión, se restaura la posición previa
+        barco->GetImagen()->setPos(prevpos);
+        break;
+        }
+    }
 
-    barco->GetImagen()->setPos(pos); // Establecer nueva posición del barco
 }
 
 
@@ -243,50 +250,44 @@ void Nivel1::Actualizarjuego()
     Fondo->setPos(Fondo->pos().x(),Fondo->pos().y()+1);
     Fondo->setZValue(-1);
 
-    if (barco->getVida() == 0 || Fondo->pos().y() == 0)
+    if (barco->getVida() <= 0 || Fondo->pos().y() == 0)
     {
         Actualizar.stop();
         tiempocreacionbarco.stop();
         tiempocreacionavion.stop();
         tiempocreacionbomba.stop();
-        if(barco->getVida() == 0)
+
+        for (int i = 0; i < barcosaliados.length(); i++)
         {
-            Actualizar.stop();
-            tiempocreacionbarco.stop();
-            tiempocreacionavion.stop();
-            tiempocreacionbomba.stop();
-
-            for (int i = 0; i < barcosaliados.length(); i++)
-            {
-                Escena->removeItem(barcosaliados[i]->GetImagen());
-                delete barcosaliados[i];
-                barcosaliados.removeAt(i);
-            }
-
-
-            for (int i = 0; i < aviones.length(); i++)
-            {
-                Escena->removeItem(aviones[i]->GetImagen());
-                delete aviones[i];
-                aviones.removeAt(i);
-            }
-
-
-            for (int i = 0; i < bombasE.length(); i++)
-            {
-                Escena->removeItem(bombasE[i]->GetImagen());
-                delete bombasE[i];
-                bombasE.removeAt(i);
-
-            }
+            Escena->removeItem(barcosaliados[i]->GetImagen());
+            delete barcosaliados[i];
+            barcosaliados.removeAt(i);
         }
+
+
+        for (int i = 0; i < aviones.length(); i++)
+        {
+            Escena->removeItem(aviones[i]->GetImagen());
+            delete aviones[i];
+            aviones.removeAt(i);
+        }
+
+
+        for (int i = 0; i < bombasE.length(); i++)
+        {
+            Escena->removeItem(bombasE[i]->GetImagen());
+            delete bombasE[i];
+            bombasE.removeAt(i);
+
+        }
+
 
         Fondofinal = new QGraphicsPixmapItem(QPixmap("C:\\Users\\NICOLAS\\Downloads\\fin del juego.jpg"));
         Escena->addItem(Fondofinal);
         Fondofinal->setPos(0,0);
         Fondofinal->setZValue(1);
 
-        if(barco->getVida() == 0)
+        if(barco->getVida() <= 0)
         {
             QMessageBox::information(nullptr, "Fin del Juego", "PERDISTE, SUERTE PARA LA PROXIMA");
         }
